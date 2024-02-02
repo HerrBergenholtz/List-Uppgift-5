d36c711dfdfec941dbd95b7dc1ab15d8af8919f7
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-02 13:30:21 +0100

    Add project files.

---
+++ List-Uppgift-5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> löner = new();
+
+            int input = 1;
+
+            while (input != 0)
+            {
+                Console.WriteLine("Skriv in löner, avlsuta med att skriva in 0");
+                Console.WriteLine();
+                Console.Write("Lön: ");
+                input = input ;
+            }
+        }
+    }
+}
