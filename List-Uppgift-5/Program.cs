adcfced0e945153083bc9e046149aabe6a040744
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-05 22:40:13 +0100

    Klart

--- List-Uppgift-5/Program.cs
+++ List-Uppgift-5/Program.cs
@@ -9,13 +9,45 @@ namespace Uppgift_5
             List<int> löner = new();
 
             int input = 1;
+            Console.WriteLine("Skriv in löner, avlsuta med att skriva in 0");
 
             while (input != 0)
             {
-                Console.WriteLine("Skriv in löner, avlsuta med att skriva in 0");
                 Console.WriteLine();
                 Console.Write("Lön: ");
-                input = input ;
+                input = int.Parse(Console.ReadLine());
+
+                if (input > 0) löner.Add(input);
+                else break;
+            }
+
+            Console.WriteLine($"Medellönen är: {Medel(löner)}");
+            Console.WriteLine($"Medianlönen är: {Median(löner)}");
+
+            Console.ReadKey();
+        }
+
+        static float Medel(List<int> löner)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < löner.Count; i++)
+            {
+                sum += löner[i];
+            }
+
+            return (float) sum / löner.Count;
+        }
+
+        static float Median (List<int> löner)
+        {
+            if (löner.Count % 2 == 0)
+            {
+                return (float) (löner[löner.Count / 2] + löner[löner.Count / 2 - 1]) / 2;
+            }
+            else
+            {
+                return löner[löner.Count / 2];
             }
         }
     }
